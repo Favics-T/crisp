f36d494af5a12e1b449e05c63d89c3dd53cf855a
unknown <taiwokolapo533@gmail.com>
2025-06-13 23:45:14 +0100

    a litlle design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Chat from './Chat'
 
 const App = () => {
   return (
-    <div className='bg-[#010713] flex flex-col justify-center items-center  text-white py-10'>
+    <div className='bg-[#010713] flex flex-col justify-center items-center  text-white py-10 px-8'>
       <Chat />
     </div>
   )
